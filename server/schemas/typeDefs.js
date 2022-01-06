6eefa82dedb9c0c38edd37b4d227ea4affe46225
Greg Zaragoza <greg.g.zaragoza@gmail.com>
2022-01-06 02:47:29 -0600

    Edited data types in typeDefs.js

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,33 +1,29 @@
 const { gql } = require('apollo-server-express');
 
 const typeDefs = gql`
-//   type User {
-//     _id: ID
-//     username: String
-//     email: String
-//     password: String
-//     thoughts: [Thought]!
-//   }
+  type User {
+    _id: ID
+    username: String
+    email: String
+    password: String
+    bookCount: Int
+    savedBooks: [Book]
+  }
 
-//   type Thought {
-//     _id: ID
-//     thoughtText: String
-//     thoughtAuthor: String
-//     createdAt: String
-//     comments: [Comment]!
-//   }
+  type Book {
+    _id: ID
+    bookId: String
+    authors: String
+    description: String
+    title: String!
+    image: String
+    link: String
+  }
 
-//   type Comment {
-//     _id: ID
-//     commentText: String
-//     commentAuthor: String
-//     createdAt: String
-//   }
-
-//   type Auth {
-//     token: ID!
-//     user: User
-//   }
+  type Auth {
+    token: ID!
+    user: User
+  }
 
 //   type Query {
 //     users: [User]
@@ -36,18 +32,18 @@ const typeDefs = gql`
 //     thought(thoughtId: ID!): Thought
 //   }
 
-//   type Mutation {
-//     addUser(username: String!, email: String!, password: String!): Auth
-//     login(email: String!, password: String!): Auth
-//     addThought(thoughtText: String!, thoughtAuthor: String!): Thought
-//     addComment(
-//       thoughtId: ID!
-//       commentText: String!
-//       commentAuthor: String!
-//     ): Thought
-//     removeThought(thoughtId: ID!): Thought
-//     removeComment(thoughtId: ID!, commentId: ID!): Thought
-//   }
+  type Mutation {
+    addUser(username: String!, email: String!, password: String!): Auth
+    login(email: String!, password: String!): Auth
+    // addThought(thoughtText: String!, thoughtAuthor: String!): Thought
+    // addComment(
+    //   thoughtId: ID!
+    //   commentText: String!
+    //   commentAuthor: String!
+    // ): Thought
+    // removeThought(thoughtId: ID!): Thought
+    // removeComment(thoughtId: ID!, commentId: ID!): Thought
+  }
 `;
 
 module.exports = typeDefs;
