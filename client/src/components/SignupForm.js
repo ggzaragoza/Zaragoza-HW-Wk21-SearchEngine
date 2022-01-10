1a6b1727e0355fd3e0c9a840847ea8987b408a43
Greg Zaragoza <greg.g.zaragoza@gmail.com>
2022-01-10 05:33:03 -0600

    Removed unused query resolvers in resolvers.js

--- client/src/components/SignupForm.js
+++ client/src/components/SignupForm.js
@@ -1,20 +1,20 @@
 import React, { useState } from 'react';
 import { Form, Button, Alert } from 'react-bootstrap';
 
-import { useMutation } from '@apollo/client';
+import { useMutation } from '@apollo/react-hooks';
 import { ADD_USER } from '../utils/mutations';
 
 import Auth from '../utils/auth';
 
 const SignupForm = () => {
+  const [addUser, { error, data }] = useMutation(ADD_USER);
+
   // set initial form state
   const [userFormData, setUserFormData] = useState({ username: '', email: '', password: '' });
   // set state for form validation
   const [validated] = useState(false);
   // set state for alert
   const [showAlert, setShowAlert] = useState(false);
-
-  const [addUser, { error, data }] = useMutation(ADD_USER);
 
   const handleInputChange = (event) => {
     const { name, value } = event.target;
--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -4,12 +4,6 @@ const { signToken } = require('../utils/auth');
 
 const resolvers = {
   Query: {
-    users: async () => {
-      return User.find().populate('savedBooks');
-    },
-    user: async (parent, { username }) => {
-      return User.findOne({ username }).populate('savedBooks');
-    },
     me: async (parent, args, context) => {
       if (context.user) {
         return User.findOne({ _id: context.user._id }).populate('savedBooks');
